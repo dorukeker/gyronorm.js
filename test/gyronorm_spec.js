52d6722de6003e0cd18c1dff65647539f99f5721
Ewout Thieme <ewout.thieme@gmail.com>
2015-02-22 12:40:29 +0100

    Refactored factory function internals.   * Extracted the private variables and set them up in the GyroNorm constructor function.   * Removed options argument from init function.   * Replaced all use of factory function global private variables with instance variables.   * Updated .init() function spec to check if _dm and _do have been set up.   * Changed log() to be an instance function and renamed it to _log().

--- test/gyronorm_spec.js
+++ test/gyronorm_spec.js
@@ -10,11 +10,22 @@ describe('GyroNorm', function() {
   });
 
   describe('.init', function() {
-    it('returns a promise', function(done) {
-      var gn = new GyroNorm();
-      var initPromise = gn.init();
+    var gn = new GyroNorm();
+    var initPromise = gn.init();
+
+    it('should set up a device orientation controller', function(done) {
       expect(initPromise)
         .to.eventually.be.fulfilled
+        .and.have.property('_do')
+        .to.not.equal(null)
+        .and.notify(done);
+    });
+
+    it('should set up a device motion controller', function(done) {
+      expect(initPromise)
+        .to.eventually.be.fulfilled
+        .and.have.property('_dm')
+        .to.not.equal(null)
         .and.notify(done);
     });
   });
