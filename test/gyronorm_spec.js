a30def392b140ef18c83f392d9ec1fa6a6d3df77
Ewout Thieme <ewout.thieme@gmail.com>
2015-02-22 15:35:38 +0100

    Added test cases for constructor.

--- test/gyronorm_spec.js
+++ test/gyronorm_spec.js
@@ -4,8 +4,60 @@ mocha.timeout(10000);
 describe('GyroNorm', function() {
 
   describe('constructor', function() {
-    it('should be a function', function () {
+    it('should be a function', function() {
       expect(GyroNorm).to.be.a('function');
+    });
+
+    context('when no options are provided', function() {
+      var gn = new GyroNorm();
+
+      describe('_frequency', function() {
+        it('should be initialized with the default value', function() {
+          expect(gn)
+            .to.have.property('_frequency')
+            .that.equals(50);
+        });
+      });
+
+      describe('_gravityNormalized', function() {
+        it('should be initialized with the default value', function() {
+          expect(gn)
+            .to.have.property('_gravityNormalized')
+            .that.equals(true);
+        });
+      });
+
+      describe('_orientationBase', function() {
+        it('should be initialized with the default value', function() {
+          expect(gn)
+            .to.have.property('_orientationBase')
+            .that.equals(GyroNorm.GAME);
+        });
+      });
+
+      describe('_decimalCount', function() {
+        it('should be initialized with the default value', function() {
+          expect(gn)
+            .to.have.property('_decimalCount')
+            .that.equals(2);
+        });
+      });
+
+      describe('_screenAdjusted', function() {
+        it('should be initialized with the default value', function() {
+          expect(gn)
+            .to.have.property('_screenAdjusted')
+            .that.equals(false);
+        });
+      });
+
+      describe('_logger', function() {
+        it('should be initialied with the default value', function() {
+          expect(gn)
+            .to.have.property('_logger')
+            .that.equals(null);
+        });
+      });
     });
   });
 
