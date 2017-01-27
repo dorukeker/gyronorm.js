48eb195f9f58120a0af99c0b5b976f97927b45bf
Doruk Eker <doruk@dorukeker.com>
2017-01-27 18:06:20 +0100

    New build files

--- dist/gyronorm.complete.js
+++ dist/gyronorm.complete.js
@@ -1,108 +1,3 @@
-(function() {
-
-  'use strict';
-
-  var Promise = function() {
-    this.thenTargets = [];
-    this.pending = true;
-  };
-
-  var isPromise = function(promise) {
-    return promise && promise instanceof Promise;
-  }
-
-  var isPseudoPromise = function(promise) {
-    return promise && typeof promise.then == 'function';
-  }
-
-  Promise.prototype.resolve = function(promise, value) {
-    if (promise === value) {
-      throw new TypeError('resolve: arguments cannot be the same object')
-    }
-    if (promise === value) {
-      throw new TypeError('resolve: arguments cannot be the same object')
-    }
-    if (isPromise(value) || isPseudoPromise(value)) {
-      value.then(promise.fulfil.bind(promise), promise.reject.bind(promise))
-    } else {
-      promise.fulfil(value);
-    }
-  };
-
-  Promise.prototype.handleThenTargets = function() {
-    var callbackResult;
-    var callback;
-    var value;
-    var i;
-
-    for (i=0;i<this.thenTargets.length;++i) {
-      if (this.fulfilled) {
-        callback = this.thenTargets[i].onFulfilled;
-        value = this.value;
-      }
-      if (this.rejected) {
-        callback = this.thenTargets[i].onRejected;
-        value = this.reason;
-      }
-      try {
-        if (callback && typeof callback === 'function') {
-          callbackResult = callback.apply(undefined, value);
-        } else {
-          callbackResult = this;
-        }
-        this.resolve(this.thenTargets[i], callbackResult);
-      }
-      catch(err) {
-        this.thenTargets[i].reject(err);
-      }
-    }
-    this.thenTargets = [];
-  };
-
-  Promise.prototype.handleThen = function() {
-    if (!this.pending) {
-      this.handleThenTargets();
-    }
-  };
-
-  Promise.prototype.then = function(onFulfilled, onRejected) {
-    var thenResult = new Promise();
-    // The execution of then is asynchronous so we need to have this info available later.
-    thenResult.onFulfilled = onFulfilled;
-    thenResult.onRejected = onRejected;
-    this.thenTargets.push(thenResult);
-    setTimeout(this.handleThen.bind(this),0);
-    return thenResult;
-  };
-
-  Promise.prototype.fulfil = function() {
-    var i;
-    var linkedPromise;
-    if (this.rejected) {
-      return;
-    }
-    this.fulfilled = true;
-    this.pending = false;
-    this.value = arguments;
-
-    this.handleThenTargets();
-  };
-
-  Promise.prototype.reject = function() {
-    var i;
-    var linkedPromise;
-    if (this.fulfilled) {
-      return;
-    }
-    this.reason = arguments;
-    this.rejected = true;
-    this.pending = false;
-    this.handleThenTargets();
-  }
-
-  this.Promise = Promise;
-
-}).call(this);
 /**
  *
  * FULL TILT
@@ -1579,9 +1474,9 @@ window.FULLTILT = FULLTILT;
 /**
 * JavaScript project for accessing and normalizing the accelerometer and gyroscope data on mobile devices
 *
-* @author Doruk Eker <dorukeker@gmail.com>
+* @author Doruk Eker <doruk@dorukeker.com>
 * @copyright Doruk Eker <http://dorukeker.com>
-* @version 2.0.4
+* @version 2.0.5
 * @license MIT License | http://opensource.org/licenses/MIT
 */
 
