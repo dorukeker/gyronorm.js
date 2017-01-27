04161096e47c96acec4fca5dbe7e4757f6fb802a
Doruk Eker <doruk@dorukeker.com>
2017-01-27 18:03:39 +0100

    Promise polyfilled removed from the complete version. (See readme.md file for details) - version number and info fixed on several files

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,22 +10,22 @@ module.exports = function(grunt) {
         separator: '\n'
       },
       core: {
-        src: ['bower_components/fulltilt/dist/fulltilt.js', 'lib/gyronorm.js'],
+        src: ['lib/gyronorm.js'],
         dest: 'dist/gyronorm.js'
       },
       complete: {
-        src: ['bower_components/promise/promise.js', 'bower_components/fulltilt/dist/fulltilt.js', 'lib/gyronorm.js'],
+        src: ['bower_components/fulltilt/dist/fulltilt.js', 'lib/gyronorm.js'],
         dest: 'dist/gyronorm.complete.js'
       },
       complete_minified: {
-        src: ['bower_components/es6-promise/es6-promise.min.js', 'bower_components/fulltilt/dist/fulltilt.min.js', 'dist/gyronorm.min.js'],
+        src: ['bower_components/fulltilt/dist/fulltilt.min.js', 'dist/gyronorm.min.js'],
         dest: 'dist/gyronorm.complete.min.js'
       }
     },
     uglify: {
       options: {
         // the banner is inserted at the top of the output
-        banner: '\n/* gyronorm.js v2.0.4 - https://github.com/dorukeker/gyronorm.git*/\n'
+        banner: '\n/* gyronorm.js v2.0.5 - https://github.com/dorukeker/gyronorm.git*/\n'
       },
       dist: {
         files: {
